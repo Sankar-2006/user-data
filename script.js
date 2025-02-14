c6f6981da8d0692f41b4087f189e3974c378f952
sankar-2006 <umasankarg2006@gmail.com>
2025-02-14 22:03:20 +0530

    added functions

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const store = document.querySelector(".navStore");
-const load = document.querySelector(".navLoad");
-const button = document.querySelector(".form");
+const navStore = document.querySelector(".navStore");
+const navLoad = document.querySelector(".navLoad");
+const store = document.querySelector(".store");
+const load = document.querySelector(".load");
+const form = document.querySelector(".form");
+const button = document.querySelector(".submit");
 
-button.addEventListener("submit", (e) => {
+navStore.addEventListener("click", () => {
+  load.style.display = "none";
+  store.style.display = "block";
+});
+
+navLoad.addEventListener("click", () => {
+  store.style.display = "none";
+  load.style.display = "block";
+});
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const data = {
     name: document.querySelector(".userName").value,
     email: document.querySelector(".userEmail").value,
   };
-  console.log("Submitted");
+  fetch("https://localhost:3001", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(data => alert("The data has been sent! :)"))
+    .catch(err => alert("Data has not sent :("))
 });

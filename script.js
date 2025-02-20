33255ae5bddfb34fca43850d0cae5a968680f237
sankar-2006 <umasankarg2006@gmail.com>
2025-02-20 19:28:12 +0530

    altered frontend

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-const navStore = document.querySelector(".navStore");
-const navLoad = document.querySelector(".navLoad");
 const store = document.querySelector(".store");
 const load = document.querySelector(".load");
 const form = document.querySelector(".form");
 const button = document.querySelector(".submit");
-
-navStore.addEventListener("click", () => {
-  load.style.display = "none";
-  store.style.display = "block";
-});
-
-navLoad.addEventListener("click", () => {
-  store.style.display = "none";
-  load.style.display = "block";
-});
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   const data = {
-//     name: document.querySelector(".userName").value,
-//     email: document.querySelector(".userEmail").value,
-//   };
-// });
 
 button.addEventListener("click", async () => {
 
@@ -30,7 +9,6 @@ button.addEventListener("click", async () => {
     name: document.querySelector(".userName").value,
     email: document.querySelector(".userEmail").value,
   };
-  // console.log(JSON.stringify(data))
 
   const response = await fetch("http://localhost:8080/", {
     method: "POST",
@@ -39,5 +17,8 @@ button.addEventListener("click", async () => {
     },
     body: JSON.stringify(data),
   });
+
   const result = await response.text();
+  alert(result);
+
 });
